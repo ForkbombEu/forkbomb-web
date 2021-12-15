34a02473ef0770290b9aae7eede25a6bfcc051b2
Puria Nafisi Azizi <puria@dyne.org>
2021-12-15 13:39:22 +0100

    Add some SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import SEO from '../next-seo.config';
+import { DefaultSeo } from 'next-seo'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <DefaultSeo {...SEO} />
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
